d34e21776e653598fd4c2ec4d930c2a213999527
ElianFernandez <elianf321@gmail.com>
2026-05-13 21:02:11 -0300

    Home demo, fix templates reset password

--- app/urls.py
+++ app/urls.py
@@ -14,20 +14,19 @@ urlpatterns = [
 
     # Recuperación de contraseña (Utilizacion de auth_views que implementa Django)
     path('password-reset/', auth_views.PasswordResetView.as_view(
-        template_name='registration/password_reset_form.html',
+        template_name='app/password_reset_form.html',
         email_template_name='app/password_reset_email.html',
-        subject_template_name='registration/password_reset_subject.txt',
         success_url=reverse_lazy('password_reset_done')
     ), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='registration/password_reset_done.html'
+        template_name='app/password_reset_done.html'
     ), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='registration/password_reset_confirm.html',
+        template_name='app/password_reset_confirm.html',
         success_url=reverse_lazy('password_reset_complete')
     ), name='password_reset_confirm'),
     path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='registration/password_reset_complete.html'
+        template_name='app/password_reset_complete.html'
     ), name='password_reset_complete'),
 
     # Vistas protegidas
